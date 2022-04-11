cab9a7a58301d2e04308d95321e256894c1a1e65
Sakshi Pathak <sakshipathak0710@gmail.com>
2022-04-11 13:49:46 +0530

    My assignment

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,7 +1,6 @@
 (function () {
 
-var names = ["Sakshi", "Johnny", "John", "Zeba", "Jenish", "Jolly", "Jenifer", "Rahul", "Rohan", "Jiya"];
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 var firstLetter = names[i].charAt(0).toLowerCase();
 if (firstLetter === 'j') {
